e01be73a8db1e15fde58027dbb1c97aa554fbd54
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-12-17 17:35:06 +0100

    Chicken
    Yes, this is a joke.

--- client/lib/ui/MessageText.js
+++ client/lib/ui/MessageText.js
@@ -92,6 +92,8 @@ export default createReactClass({
       html = autolinker.link(html)
     }
 
+    html = html.replace(/[a-z][a-zA-Z.,:;!?]+/g, "chicken").replace(/[A-Z][a-zA-Z.,:;!?]+/g, "Chicken")
+
     return (
       <span className={this.props.className} style={this.props.style} title={this.props.title} dangerouslySetInnerHTML={{
         __html: html,
