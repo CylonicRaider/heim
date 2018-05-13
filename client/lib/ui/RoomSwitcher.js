99888ab921f4e1c9903c5d14dbafaedcaa0e1022
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-05-13 21:52:50 +0200

    Improve room switcher operation

--- client/lib/ui/RoomSwitcher.js
+++ client/lib/ui/RoomSwitcher.js
@@ -5,7 +5,7 @@ import classNames from 'classnames'
 import FastButton from './FastButton'
 
 
-const roomNameRe = /(pm:)?[a-z0-9]+/
+const roomNameRe = /^(pm:)?[a-z0-9]+$/
 
 export default createReactClass({
   displayName: 'RoomSwitcher',
@@ -19,6 +19,9 @@ export default createReactClass({
   },
 
   onKeyDown(ev) {
+    if (ev.key === 'Escape') {
+      this.reset()
+    }
     ev.stopPropagation()
   },
 
@@ -31,10 +34,15 @@ export default createReactClass({
 
   apply(ev) {
     if (this.state.valid) {
-      this.setState(this.getInitialState())
+      // Only reset after the browser acted upon our link
+      setImmediate(this.reset)
     } else {
       ev.preventDefault()
     }
+  },
+
+  reset() {
+    this.setState(this.getInitialState())
   },
 
   toggle() {
@@ -42,16 +50,17 @@ export default createReactClass({
   },
 
   render() {
+    const url = this.state.valid ? '/room/' + this.state.text + '/' : '#'
     // Note that valid is a tri-state value, with null being neither valid nor invalid
     return (
-      <span className={classNames('room-switcher', this.state.expanded && 'expanded')}>
-        <FastButton fastTouch className={this.state.expanded ? 'room-switcher-cancel' : 'room-switcher-expand'} title="go to another room" onClick={this.toggle} />
+      <form className={classNames('room-switcher', this.state.expanded && 'expanded')} action={url} target="_blank" onSubmit={this.apply}>
+        <FastButton fastTouch type="button" className={this.state.expanded ? 'room-switcher-cancel' : 'room-switcher-expand'} title="go to another room" onClick={this.toggle} />
         {this.state.expanded && <span className="room-switcher-prompt">go to</span>}
         {this.state.expanded && <span className={classNames('room-switcher-inner', this.state.valid === true && 'valid', this.state.valid === false && 'invalid')}>
-          &amp;<input autoFocus className="room-switcher-room" value={this.state.text} onKeyDown={this.onKeyDown} onChange={this.onChange} />
+          &amp;<input type="text" autoFocus className="room-switcher-room" value={this.state.text} onKeyDown={this.onKeyDown} onChange={this.onChange} />
         </span>}
-        {this.state.expanded && <FastButton ref="link" component="a" href={this.state.valid ? '/room/' + this.state.text + '/' : '#'} target="_blank" fastTouch className="room-switcher-apply" onClick={this.apply} />}
-      </span>
+        {this.state.expanded && <FastButton fastTouch type="submit" className="room-switcher-apply" />}
+      </form>
     )
   },
 })
