3a97ded51e7c6e155d0d561b9bfe8c7c64298a36
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-01-07 21:01:35 +0100

    Revise about page

--- client/site/about.js
+++ client/site/about.js
@@ -6,7 +6,7 @@ import heimURL from '../lib/heimURL'
 module.exports = (
   <MainPage title="about euphoria" className="about">
     <div className="primary">
-      <h1>what's euphoria?</h1>
+      <h1>What's euphoria?</h1>
       <section className="letter">
         <h3>Euphoria is chat for all, in communities that matter.</h3>
         <p>Hello! We're building a platform for chat rooms you care about.</p>
@@ -17,48 +17,48 @@ module.exports = (
             <span className="label">Got a question, or want to learn more?</span>
             <a className="chat-with-us green-button" href={heimURL('/room/welcome/')}>chat with us &raquo;</a>
           </p>
-          <p className="signature">
-            &mdash; <FauxNick nick="intortus" />, <FauxNick nick="chromakode" />, and <FauxNick nick="greenie" />
-          </p>
         </div>
       </section>
     </div>
     <section>
-      <h2>our values (in emoji form)</h2>
+      <h2>Our values (in emoji form)</h2>
       <FancyLogo />
       <ul className="values">
         <li className="welcoming">
-          <h4>euphoria is welcoming</h4>
-          <p>friendly to newcomers, easy to join, and safe.</p>
+          <h4>Euphoria is welcoming</h4>
+          <p>Friendly to newcomers, easy to join, and safe.</p>
         </li>
         <li className="diverse">
-          <h4>euphoria is diverse</h4>
-          <p>we strive for tolerance, fairness, and accessibility.</p>
+          <h4>Euphoria is diverse</h4>
+          <p>We strive for tolerance, fairness, and accessibility.</p>
         </li>
         <li className="meaningful">
-          <h4>euphoria is informal yet meaningful</h4>
-          <p>everyone deserves respect, empathy, and understanding.</p>
+          <h4>Euphoria is informal yet meaningful</h4>
+          <p>Everyone deserves respect, empathy, and understanding.</p>
         </li>
       </ul>
-      <p>for more details, check out <a href={heimURL('/about/values')}>Euphoria's Values</a> statement.</p>
+      <p>For more details, check out <a href={heimURL('/about/values')}>Euphoria's Values</a> statement.</p>
     </section>
     <section>
-      <h2>euphoria is open</h2>
+      <h2>Euphoria is open</h2>
       <h3>We believe that online community platforms should be open source.</h3>
       <p>Our chat server, Heim, is <a href="https://github.com/euphoria-io/heim">available on GitHub</a>. Join our development chat in <a href={heimURL('/room/heim')}>&heim</a>.</p>
     </section>
     <section className="who">
-      <h2>who we are</h2>
-      <h3>We're a small team who care deeply about online socialization and citizenship.</h3>
-      <div className="messages wrap">
-        <FauxMessage sender="intortus" message="hi, I'm logan. I'm an erstwhile motorcycle racer and one of euphoria's programmers. you can meet new users with me in &welcome or discuss backend development with me in &heim." />
-        <FauxMessage sender="chromakode" message="hey, I'm Max! I live in San Francisco, where I work on Euphoria's user interface and design. you can often find me jamming in &music or working in &heim." />
-        <FauxMessage sender="greenie" message="oh hai, I’m Kris. I live with too many cats in the deep forest of Vermont. when I’m not busy getting into moose-caused traffic jams, I do community stuff for Euphoria. I tend to be found linking articles in &space and playing bluegrass tunes in &music." />
-        <FauxMessage sender="ezzie" message="hi, I'm ezzie, the office dog!" embed={heimURL('/static/ezzie.jpg')}>
-          <div className="replies">
-            <FauxMessage sender="ezzie" message="you can find more photos of me in the room &ezziethedog." />
-          </div>
-        </FauxMessage>
+      <h2>Who we were</h2>
+      <p>This section lists the original creators of Euphoria, who have since left to pursue other projects.</p>
+      <div className="old-who">
+        <h3>We're a small team who care deeply about online socialization and citizenship.</h3>
+        <div className="messages wrap">
+          <FauxMessage sender="intortus" message="hi, I'm logan. I'm an erstwhile motorcycle racer and one of euphoria's programmers. you can meet new users with me in &welcome or discuss backend development with me in &heim." />
+          <FauxMessage sender="chromakode" message="hey, I'm Max! I live in San Francisco, where I work on Euphoria's user interface and design. you can often find me jamming in &music or working in &heim." />
+          <FauxMessage sender="greenie" message="oh hai, I’m Kris. I live with too many cats in the deep forest of Vermont. when I’m not busy getting into moose-caused traffic jams, I do community stuff for Euphoria. I tend to be found linking articles in &space and playing bluegrass tunes in &music." />
+          <FauxMessage sender="ezzie" message="hi, I'm ezzie, the office dog!" embed={heimURL('/static/ezzie.jpg')}>
+            <div className="replies">
+              <FauxMessage sender="ezzie" message="you can find more photos of me in the room &ezziethedog." />
+            </div>
+          </FauxMessage>
+        </div>
       </div>
     </section>
   </MainPage>
