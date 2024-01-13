4ec1e0a4ecb9f3cdef096c1c3c4014335961d69e
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-01-13 22:26:49 +0100

    Gather backend source files in one directory
    If the client doesn't spray its files around the repository root, why
    should the server?

--- server/backend/run.sh
+++ server/backend/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 go install \
-    -ldflags "-X main.version=$(git --git-dir=src/euphoria.leet.nu/heim/.git rev-parse HEAD)" \
+    -ldflags "-X main.version=$(git --git-dir=/srv/heim/.git rev-parse HEAD)" \
     euphoria.leet.nu/heim/heimctl
 
 export PATH=/go/bin:"$PATH"
