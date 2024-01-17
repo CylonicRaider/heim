8b1768d3e2e77f43fd76506793c2a9d9cd9be349
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-01-17 22:27:03 +0100

    Rework heimlich resource cleanup and error handling

--- server/heimlich/heimlich.go
+++ server/heimlich/heimlich.go
@@ -11,41 +11,67 @@ import (
 	"syscall"
 )
 
+// yes, one could write out all possible permutations of the error handling
+// and closing paths below manually, but that would be more bug-prone than
+// this simple but dynamic approach
+type closeOnce struct {
+	f io.Closer
+}
+
+func (co *closeOnce) Close() error {
+	if co.f == nil {
+		return nil
+	}
+	err := co.f.Close()
+	co.f = nil
+	return err
+}
+
+// does not continue closing after the first error; use defer for that
+func closeUntilError(cos ...*closeOnce) error {
+	for _, c := range cos {
+		if err := c.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func swallow(self string, binary string, paths []string) error {
 	sf, err := os.Open(self)
 	if err != nil {
 		return fmt.Errorf("open %s: %s", self, err)
 	}
+	defer sf.Close()
+
 	hzp := binary + ".hzp"
 	f, err := os.OpenFile(hzp, os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		return fmt.Errorf("open %s: %s", hzp, err)
 	}
+	fc := closeOnce{f}
+	defer fc.Close()
+
 	n, err := io.Copy(f, sf)
 	if err != nil {
-		sf.Close()
 		return fmt.Errorf("copy %s->%s: %s", self, hzp, err)
-	}
-	if err := sf.Close(); err != nil {
-		return fmt.Errorf("close %s: %s", self, err)
 	}
 
 	zw := zip.NewWriter(f)
 	zw.SetOffset(n)
+	zwc := closeOnce{zw}
+	defer zwc.Close()
+
 	if err := swallowFile(zw, binary); err != nil {
-		f.Close()
 		return fmt.Errorf("swallow %s: %s", binary, err)
 	}
 	for _, path := range paths {
 		if err := swallowFile(zw, path); err != nil {
-			f.Close()
 			return fmt.Errorf("swallow %s: %s", path, err)
 		}
 	}
-	if err := zw.Close(); err != nil {
-		return err
-	}
-	return f.Close()
+
+	return closeUntilError(&zwc, &fc)
 }
 
 func swallowFile(zw *zip.Writer, path string) error {
@@ -54,6 +80,8 @@ func swallowFile(zw *zip.Writer, path string) error {
 	if err != nil {
 		return fmt.Errorf("open %s: %s", path, err)
 	}
+	defer f.Close()
+
 	fi, err := f.Stat()
 	if err != nil {
 		return fmt.Errorf("stat %s: %s", path, err)
@@ -63,6 +91,7 @@ func swallowFile(zw *zip.Writer, path string) error {
 		return fmt.Errorf("fileinfoheader %s: %s", path, err)
 	}
 	fh.Name = path
+
 	w, err := zw.CreateHeader(fh)
 	if err != nil {
 		return fmt.Errorf("create header %s: %s", path, err)
@@ -70,6 +99,7 @@ func swallowFile(zw *zip.Writer, path string) error {
 	if _, err := io.Copy(w, f); err != nil {
 		return fmt.Errorf("archive %s: %s", path, err)
 	}
+
 	return nil
 }
 
@@ -80,6 +110,8 @@ func extract(hzp string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+	defer f.Close()
+
 	fi, err := f.Stat()
 	if err != nil {
 		return "", err
@@ -88,37 +120,44 @@ func extract(hzp string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	for _, file := range zr.File {
-		extractFile(root, file)
+		if err := extractFile(root, file); err != nil {
+			return "", err
+		}
 	}
+
 	return filepath.Abs(filepath.Join(root, zr.File[0].Name))
 }
 
 func extractFile(root string, file *zip.File) error {
 	fmt.Printf("  extracting %s ...\n", file.Name)
+
 	path := filepath.Join(root, file.Name)
 	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 		return err
 	}
+
 	r, err := file.Open()
 	if err != nil {
 		return err
 	}
+	rc := closeOnce{r}
+	defer rc.Close()
+
 	perms := file.FileInfo().Mode().Perm()
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, perms)
 	if err != nil {
-		r.Close()
 		return err
 	}
+	fc := closeOnce{f}
+	defer fc.Close()
+
 	if _, err := io.Copy(f, r); err != nil {
-		r.Close()
-		f.Close()
 		return err
 	}
-	if err := f.Close(); err != nil {
-		return err
-	}
-	return r.Close()
+
+	return closeUntilError(&fc, &rc)
 }
 
 func extractAndRun(hzp string, args []string, env []string) error {
@@ -126,6 +165,7 @@ func extractAndRun(hzp string, args []string, env []string) error {
 	if err != nil {
 		return err
 	}
+
 	fmt.Printf("executing: %s %s\n", exePath, strings.Join(args, " "))
 	return syscall.Exec(exePath, args, env)
 }
