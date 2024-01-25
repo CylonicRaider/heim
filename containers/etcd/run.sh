312249d77389c399f14008f30177f2970c96c35d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-01-25 23:56:55 +0100

    Update etcd container

--- containers/etcd/run.sh
+++ containers/etcd/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-ETCD_CMD="/bin/etcd -name etcd -data-dir /data -addr $HOSTNAME:4001 -bind-addr $HOSTNAME:4001 $*"
+ETCD_CMD="/bin/etcd \
+-name etcd -data-dir /data \
+-advertise-client-urls http://$HOSTNAME:4001 -listen-client-urls http://0.0.0.0:4001 \
+-initial-advertise-peer-urls http://$HOSTNAME:7001 -listen-peer-urls http://0.0.0.0:7001 \
+$*"
 echo "Running: $ETCD_CMD"
 $ETCD_CMD
