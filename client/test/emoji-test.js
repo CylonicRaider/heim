6c4eec70079ffce5d80687361cf5d4ba03bd3dbb
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-03-27 18:22:16 +0100

    Expand emoji tests

--- client/test/emoji-test.js
+++ client/test/emoji-test.js
@@ -4,13 +4,29 @@ import assert from 'assert'
 describe('emoji', () => {
   const emoji = require('../lib/emoji')
 
+  describe('nameToUnicode', () => {
+    it('translates BMP emoji', () => {
+      // U+25C0 BLACK LEFT-POINTING TRIANGLE (:arrow_backward:)
+      assert.equal(emoji.nameToUnicode('arrow_backward'), '\u25c0')
+    })
+
+    it('translates astral emoji', () => {
+      // U+1F34E RED APPLE (:apple:)
+      assert.equal(emoji.nameToUnicode('apple'), '\ud83c\udf4e')
+    })
+
+    it('does not translate custom emoji', () => {
+      assert.equal(emoji.nameToUnicode('euphoria'), null)
+    })
+  })
+
   describe('unicodeToIconID', () => {
-    it('translates regular emoji', () => {
+    it('translates BMP emoji', () => {
       // U+25B6 BLACK RIGHT-POINTING TRIANGLE (:arrow_forward:)
       assert.equal(emoji.unicodeToIconID('\u25b6'), '25b6')
     })
 
-    it('translates a non-BMP emoji', () => {
+    it('translates astral emoji', () => {
       // U+1F514 BELL (:bell:)
       assert.equal(emoji.unicodeToIconID('\ud83d\udd14'), '1f514')
     })
