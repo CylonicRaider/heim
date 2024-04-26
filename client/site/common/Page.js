c1008ce9f6d79ea88c1c0556a44db25cff4bf9a0
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-04-26 20:02:29 +0200

    Use defer instead of async for site scripts
    This leads to a more reliable execution order, in particular w.r.t. the
    DOM loading.

--- client/site/common/Page.js
+++ client/site/common/Page.js
@@ -16,12 +16,12 @@ export default function Page(props) {
         {props.heimPage && <link rel="stylesheet" type="text/css" id="css" href={heimURL('/static/main.css')} />}
         <link rel="stylesheet" type="text/css" id="css" href={heimURL('/static/site.css')} />
         {props.heimPage && <script src={heimURL('/static/raven.js')} />}
-        <script async src={heimURL('/static/fast-touch.js')} />
-        <script async src={heimURL('/static/dynamic-toc.js')} />
+        <script defer src={heimURL('/static/fast-touch.js')} />
+        <script defer src={heimURL('/static/dynamic-toc.js')} />
       </head>
       <body className={props.className}>
         {props.children}
-        {props.heimPage && <script async id="heim-js" src={heimURL('/static/main.js')} data-entrypoint={props.heimPage} />}
+        {props.heimPage && <script defer id="heim-js" src={heimURL('/static/main.js')} data-entrypoint={props.heimPage} />}
       </body>
     </html>
   )
