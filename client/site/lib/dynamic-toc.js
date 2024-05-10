0f811a01a28a7dc4353bf16361c8609d286a7107
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-05-10 19:57:26 +0200

    ES6ify site scripts and mollify linter
    They no longer need to be written in ES5 as Babel does that for us.

--- client/site/lib/dynamic-toc.js
+++ client/site/lib/dynamic-toc.js
@@ -1,91 +1,88 @@
-/* eslint-disable prefer-arrow-callback, no-var */
-(function initDynamicToc() {
-  var toc = document.querySelector('.toc')
-  var inlineAnchor = document.querySelector('#marker-toc')
-  var sidebar = document.querySelector('.sidebar')
-  var sidebarContainer = document.querySelector('.sidebar-container')
-  var isInSidebar = false
-  var selectableEntries = []
-  var curSelected = null
+const toc = document.querySelector('.toc')
+const inlineAnchor = document.querySelector('#marker-toc')
+const sidebar = document.querySelector('.sidebar')
+const sidebarContainer = document.querySelector('.sidebar-container')
+const selectableEntries = []
+let isInSidebar = false
+let curSelected = null
 
-  function parseFloatOrZero(text) {
-    return text ? parseFloat(text) : 0
+function parseFloatOrZero(text) {
+  return text ? parseFloat(text) : 0
+}
+
+function scrollToShow(node) {
+  const parent = node.offsetParent
+  if (parent === null) return
+  const nodeRect = node.getBoundingClientRect()
+  const nodeStyle = getComputedStyle(node)
+  const parentRect = parent.getBoundingClientRect()
+  const nodeTop = nodeRect.top - parseFloatOrZero(nodeStyle.scrollMarginTop)
+  const nodeBottom = nodeRect.bottom + parseFloatOrZero(nodeStyle.scrollMarginBottom)
+  if (nodeTop < parentRect.top) {
+    parent.scrollTop -= parentRect.top - nodeTop
+  } else if (nodeBottom > parentRect.bottom) {
+    parent.scrollTop += nodeBottom - parentRect.bottom
   }
+}
 
-  function scrollToShow(node) {
-    var parent = node.offsetParent
-    if (parent === null) return
-    var nodeRect = node.getBoundingClientRect()
-    var nodeStyle = getComputedStyle(node)
-    var parentRect = parent.getBoundingClientRect()
-    var nodeTop = nodeRect.top - parseFloatOrZero(nodeStyle.scrollMarginTop)
-    var nodeBottom = nodeRect.bottom + parseFloatOrZero(nodeStyle.scrollMarginBottom)
-    if (nodeTop < parentRect.top) {
-      parent.scrollTop -= parentRect.top - nodeTop
-    } else if (nodeBottom > parentRect.bottom) {
-      parent.scrollTop += nodeBottom - parentRect.bottom
-    }
+function setSelected(index) {
+  if (index === curSelected) {
+    return
   }
-
-  function setSelected(index) {
-    if (index === curSelected) {
-      return
-    }
-    if (curSelected !== null) {
-      selectableEntries[curSelected].link.classList.remove('selected')
-    }
-    curSelected = index
-    if (index !== null) {
-      var newLink = selectableEntries[index].link
-      newLink.classList.add('selected')
-      if (isInSidebar) {
-        scrollToShow(newLink)
-      }
+  if (curSelected !== null) {
+    selectableEntries[curSelected].link.classList.remove('selected')
+  }
+  curSelected = index
+  if (index !== null) {
+    const newLink = selectableEntries[index].link
+    newLink.classList.add('selected')
+    if (isInSidebar) {
+      scrollToShow(newLink)
     }
   }
+}
 
-  function updateTocLocation() {
-    if (getComputedStyle(sidebarContainer).display !== 'none') {
-      sidebar.appendChild(toc)
-      isInSidebar = true
-      updateTocCurrent()
-    } else {
-      inlineAnchor.parentNode.insertBefore(toc, inlineAnchor.nextSibling)
-      isInSidebar = false
-      setSelected(null)
-    }
+function updateTocCurrent() {
+  if (!selectableEntries.length || !isInSidebar) return
+
+  function testLocation(node) {
+    const rect = node.getBoundingClientRect()
+    const styles = getComputedStyle(node)
+    const top = rect.top - parseFloatOrZero(styles.marginTop)
+    const bottom = rect.bottom + parseFloatOrZero(styles.marginBottom)
+    if (top > 0) return -1
+    if (bottom < 0) return 1
+    return 0
   }
 
-  function updateTocCurrent() {
-    if (!selectableEntries.length || !isInSidebar) return
-
-    function testLocation(node) {
-      var rect = node.getBoundingClientRect()
-      var styles = getComputedStyle(node)
-      var top = rect.top - parseFloatOrZero(styles.marginTop)
-      var bottom = rect.bottom + parseFloatOrZero(styles.marginBottom)
-      return top > 0 ? -1 : bottom < 0 ? 1 : 0
-    }
-
-    var s = curSelected === null ? 0 : curSelected
-    for (;;) {
-      var verdict = testLocation(selectableEntries[s].heading)
-      if (verdict == 0) break
-      if (verdict == -1 && s <= 0) break
-      if (verdict == 1 && s >= selectableEntries.length - 1) break
-      if (verdict == 1 && testLocation(selectableEntries[s + 1].heading) < 0) break
-      s += verdict
-    }
-    setSelected(s)
+  let s = curSelected === null ? 0 : curSelected
+  for (;;) {
+    const verdict = testLocation(selectableEntries[s].heading)
+    if (verdict === 0) break
+    if (verdict === -1 && s <= 0) break
+    if (verdict === 1 && s >= selectableEntries.length - 1) break
+    if (verdict === 1 && testLocation(selectableEntries[s + 1].heading) < 0) break
+    s += verdict
   }
+  setSelected(s)
+}
 
-  if (!toc || !inlineAnchor || !sidebar) return
+function updateTocLocation() {
+  if (getComputedStyle(sidebarContainer).display !== 'none') {
+    sidebar.appendChild(toc)
+    isInSidebar = true
+    updateTocCurrent()
+  } else {
+    inlineAnchor.parentNode.insertBefore(toc, inlineAnchor.nextSibling)
+    isInSidebar = false
+    setSelected(null)
+  }
+}
 
-  Array.prototype.forEach.call(toc.querySelectorAll('a'), function(link) {
-    if (link.classList.contains('header-anchor') ||
-        !/^#[\w-]+/.test(link.hash))
-      return
-    var heading = document.getElementById(link.hash.substring(1))
+if (toc && inlineAnchor && sidebar) {
+  Array.prototype.forEach.call(toc.querySelectorAll('a'), (link) => {
+    if (link.classList.contains('header-anchor') || !/^#[\w-]+/.test(link.hash)) return
+    const heading = document.getElementById(link.hash.substring(1))
     selectableEntries.push({
       link: link,
       heading: heading,
@@ -96,4 +93,4 @@
   document.addEventListener('scroll', updateTocCurrent)
   updateTocLocation()
   updateTocCurrent()
-}())
+}
--- client/site/lib/fast-touch.js
+++ client/site/lib/fast-touch.js
@@ -1,12 +1,9 @@
-/* eslint-disable prefer-arrow-callback */
-(function initFastTouch(body) {
-  if ('ontouchstart' in window) {
-    body.classList.add('touch')
-    body.addEventListener('touchstart', function fastTouchStart(ev) {
-      ev.target.classList.add('touching')
-    }, false)
-    body.addEventListener('touchend', function fastTouchEnd(ev) {
-      ev.target.classList.remove('touching')
-    }, false)
-  }
-}(document.body))
+if ('ontouchstart' in window) {
+  document.body.classList.add('touch')
+  document.body.addEventListener('touchstart', (ev) => {
+    ev.target.classList.add('touching')
+  }, false)
+  document.body.addEventListener('touchend', (ev) => {
+    ev.target.classList.remove('touching')
+  }, false)
+}
