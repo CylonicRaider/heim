96863a500886ac484239fc689a49358d7f5485e9
CylonicRaider <CylonicRaider@users.noreply.github.com>
2024-05-18 23:31:56 +0200

    Expand emoji tests

--- client/test/emoji-test.js
+++ client/test/emoji-test.js
@@ -3,36 +3,42 @@ import assert from 'assert'
 
 import emoji from '../lib/heim/emoji'
 
+const referenceEmoji = [
+  { u: '\u25b6', i: '25b6', n: 'arrow_forward', d: 'BMP emoji' },
+  { u: '\ud83d\udd14', i: '1f514', n: 'bell', d: 'astral emoji' },
+  // The Unicode text is incorrect (note the missing \u200d), but this captures the current behavior.
+  { u: '\ud83d\udc41\ud83d\udde8', i: '1f441-1f5e8', n: 'eye_speech_bubble', d: 'ZWJ sequences' },
+  { u: null, i: '~plusone', n: '+1', d: 'customized Unicode emoji' },
+  { u: null, i: '~euphoria', n: 'euphoria', d: 'custom emoji' },
+]
+
+function runTests(label, callback, filter = null) {
+  describe(label, () => {
+    referenceEmoji.forEach((record) => {
+      if (filter && !filter(record)) return
+      it(`works with ${record.d}`, () => callback(record))
+    })
+  })
+}
+
 describe('emoji', () => {
-  describe('nameToUnicode', () => {
-    it('translates BMP emoji', () => {
-      // U+25C0 BLACK LEFT-POINTING TRIANGLE (:arrow_backward:)
-      assert.equal(emoji.nameToUnicode('arrow_backward'), '\u25c0')
-    })
-
-    it('translates astral emoji', () => {
-      // U+1F34E RED APPLE (:apple:)
-      assert.equal(emoji.nameToUnicode('apple'), '\ud83c\udf4e')
-    })
-
-    it('does not translate custom emoji', () => {
-      assert.equal(emoji.nameToUnicode('euphoria'), null)
-    })
-
-    it('respects customized Unicode emoji', () => {
-      assert.equal(emoji.nameToUnicode('+1'), null)
-    })
+  runTests('nameToIconID', (record) => {
+    assert.equal(emoji.nameToIconID(record.n), record.i)
   })
 
-  describe('unicodeToIconID', () => {
-    it('translates BMP emoji', () => {
-      // U+25B6 BLACK RIGHT-POINTING TRIANGLE (:arrow_forward:)
-      assert.equal(emoji.unicodeToIconID('\u25b6'), '25b6')
-    })
-
-    it('translates astral emoji', () => {
-      // U+1F514 BELL (:bell:)
-      assert.equal(emoji.unicodeToIconID('\ud83d\udd14'), '1f514')
-    })
+  runTests('iconIDToName', (record) => {
+    assert.equal(emoji.iconIDToName(record.i), record.n)
   })
+
+  runTests('iconClass', (record) => {
+    assert.ok(/^emoji emoji-[a-z0-9-]+$/.test(emoji.iconClass(record.i)))
+  })
+
+  runTests('nameToUnicode', (record) => {
+    assert.equal(emoji.nameToUnicode(record.n), record.u)
+  })
+
+  runTests('unicodeToIconID', (record) => {
+    assert.equal(emoji.unicodeToIconID(record.u), record.i)
+  }, (record) => record.u)
 })
