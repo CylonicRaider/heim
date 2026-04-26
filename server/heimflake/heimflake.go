013de1892b947e60b540a864adab9eaf09aecf65
CylonicRaider <CylonicRaider@users.noreply.github.com>
2026-04-26 23:11:21 +0200

    Allow formatting snowflakes in local time

--- server/heimflake/heimflake.go
+++ server/heimflake/heimflake.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -8,7 +9,14 @@ import (
 	"euphoria.leet.nu/heim/proto/snowflake"
 )
 
+var (
+	localMode = flag.Bool("l", false, "Convert timestamps into local timezone")
+)
+
 func formatTime(t time.Time) string {
+	if *localMode {
+		t = t.In(time.Local)
+	}
 	return t.Format("2006-01-02 15:04:05.000 Z0700")
 }
 
@@ -24,13 +32,15 @@ func reportError(err error, fatal bool) bool {
 }
 
 func main() {
-	if len(os.Args) <= 1 {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
 		sf, err := snowflake.New()
 		reportError(err, true)
 		fmt.Println(sf.String())
 		return
 	}
-	for _, s := range os.Args[1:] {
+	for _, s := range args {
 		sf, err := snowflake.NewFromString(s)
 		if reportError(err, false) {
 			continue
