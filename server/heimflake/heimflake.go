25fb05acdd753757776175d189b5274c1c585400
CylonicRaider <CylonicRaider@users.noreply.github.com>
2026-04-28 23:47:55 +0200

    Add snowflake introspection to heimflake

--- server/heimflake/heimflake.go
+++ server/heimflake/heimflake.go
@@ -10,7 +10,8 @@ import (
 )
 
 var (
-	localMode = flag.Bool("l", false, "Convert timestamps into local timezone")
+	localMode   = flag.Bool("l", false, "Convert timestamps into local timezone")
+	verboseMode = flag.Bool("v", false, "Show more details")
 )
 
 func formatTime(t time.Time) string {
@@ -18,6 +19,18 @@ func formatTime(t time.Time) string {
 		t = t.In(time.Local)
 	}
 	return t.Format("2006-01-02 15:04:05.000 Z0700")
+}
+
+func formatDetails(sf snowflake.Snowflake) string {
+	parts := sf.Split()
+	return fmt.Sprintf(`
+snowflake: %s (%d)
+time     : %s (%d)
+worker   : %d
+sequence : %d`[1:],
+		sf, uint64(sf),
+		formatTime(parts.Time), uint64(time.Duration(parts.Time.UnixNano())/time.Millisecond),
+		parts.WorkerID, parts.Sequence)
 }
 
 func reportError(err error, fatal bool) bool {
@@ -37,14 +50,25 @@ func main() {
 	if len(args) == 0 {
 		sf, err := snowflake.New()
 		reportError(err, true)
-		fmt.Println(sf.String())
+		if *verboseMode {
+			fmt.Println(formatDetails(sf))
+		} else {
+			fmt.Println(sf)
+		}
 		return
 	}
-	for _, s := range args {
+	for i, s := range args {
 		sf, err := snowflake.NewFromString(s)
 		if reportError(err, false) {
 			continue
 		}
-		fmt.Printf("%s: %s\n", sf, formatTime(sf.Time()))
+		if *verboseMode {
+			if i != 0 {
+				fmt.Println()
+			}
+			fmt.Println(formatDetails(sf))
+		} else {
+			fmt.Printf("%s: %s\n", sf, formatTime(sf.Time()))
+		}
 	}
 }
