80965ca8c233755a28a8fa398e814c17ccf48697
CylonicRaider <CylonicRaider@users.noreply.github.com>
2026-05-08 23:59:17 +0200

    Update Docker files to newer software versions

--- containers/etcd/run.sh
+++ containers/etcd/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# TODO: Migrate to newer etcd.
 ETCD_CMD="/bin/etcd \
 -name etcd -data-dir /data \
 -advertise-client-urls http://$HOSTNAME:4001 -listen-client-urls http://0.0.0.0:4001 \
 -initial-advertise-peer-urls http://$HOSTNAME:7001 -listen-peer-urls http://0.0.0.0:7001 \
+-enable-v2 \
 $*"
 echo "Running: $ETCD_CMD"
 $ETCD_CMD
